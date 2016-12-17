b4d1baecf477baa974aef62eb902389be448e10a
Nikola Lukić <luknik94@gmail.com>
2016-12-17 18:02:55 +0100

    Initial commit.

---
+++ src/main/java/com/bdzjn/xml/ParliamentOfFoolsApplication.java
@@ -0,0 +1,12 @@
+package com.bdzjn.xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParliamentOfFoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParliamentOfFoolsApplication.class, args);
+	}
+}
